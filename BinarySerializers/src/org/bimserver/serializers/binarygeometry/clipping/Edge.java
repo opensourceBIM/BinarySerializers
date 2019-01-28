b3dd69cffe8e1c8788e1350787d2241d21dd02cf
Ruben de Laat <ruben@logic-labs.nl>
2019-01-28 11:16:10 +0100

    Updated licenses

--- BinarySerializers/src/org/bimserver/serializers/binarygeometry/clipping/Edge.java
+++ BinarySerializers/src/org/bimserver/serializers/binarygeometry/clipping/Edge.java
@@ -1,23 +1,21 @@
 package org.bimserver.serializers.binarygeometry.clipping;
 
-/*
-This file is part of JHAVE -- Java Hosted Algorithm Visualization
-Environment, developed by Tom Naps, David Furcy (both of the
-University of Wisconsin - Oshkosh), Myles McNally (Alma College), and
-numerous other contributors who are listed at the http://jhave.org
-site
-JHAVE is free software: you can redistribute it and/or modify it under
-the terms of the GNU General Public License as published by the Free
-Software Foundation, either version 3 of the License, or (at your
-option) any later version.
-JHAVE is distributed in the hope that it will be useful, but WITHOUT
-ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-for more details.
-You should have received a copy of the GNU General Public License
-along with the JHAVE. If not, see:
-<http://www.gnu.org/licenses/>.
-*/
+/******************************************************************************
+ * Copyright (C) 2009-2019  BIMserver.org
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see {@literal<http://www.gnu.org/licenses/>}.
+ *****************************************************************************/
 
 import java.awt.*;
 import java.awt.geom.*;
--- BinarySerializers/src/org/bimserver/serializers/binarygeometry/clipping/PolygonClipping.java
+++ BinarySerializers/src/org/bimserver/serializers/binarygeometry/clipping/PolygonClipping.java
@@ -1,8 +1,21 @@
 package org.bimserver.serializers.binarygeometry.clipping;
 
-//works with Clipping.java to test the Sutherland-Hodgeman Clipping Algorithm
-//clipping algorithm was inspired by the open-source project JHAVE
-//the rest of the JHAVE project is GPL-licensed and NOT licensed by the sample code base
+/******************************************************************************
+ * Copyright (C) 2009-2019  BIMserver.org
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see {@literal<http://www.gnu.org/licenses/>}.
+ *****************************************************************************/
 
 import java.awt.Graphics;
 import java.awt.Graphics2D;
